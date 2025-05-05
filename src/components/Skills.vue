0f38fc584aca21a225cc6fa9deb48a7c3950b308
cchassaing <c.chassaing@cyim.com>
2025-05-05 14:39:18 +0200

    change page skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -30,14 +30,19 @@
         {{ skill.experience }}
       </div>
     </div>
+    <div ref="scrollAnchor"></div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, nextTick } from 'vue';
-  import HeaderPage from './HeaderPage.vue'
+import { ref, onMounted, nextTick, computed } from 'vue';
+  import HeaderPage from './HeaderPage.vue';
+  import { useI18n } from "vue-i18n";
 
-  const skills = ref([
+  const scrollAnchor = ref(null)
+  const { t } = useI18n();
+
+  const skills = computed(() => [
     {
       name: 'Drupal',
       logo: [
@@ -46,7 +51,7 @@ import { ref, onMounted, nextTick } from 'vue';
         '/assets/images/icon/bootstrap.png'
       ],
       percentage: 90,
-      experience: "Utilisé depuis 6 ans sur des projets pour des sociétés scientifiques ainsi que pour la réalisation de projets destinés à des congrès scientifiques.",
+      experience: t('skillsPage.drupalExp'),
       startBlock: 'slide-left'
     },
     {
@@ -56,7 +61,7 @@ import { ref, onMounted, nextTick } from 'vue';
         '/assets/images/icon/vuetify.png'
       ],
       percentage: 80,
-      experience: "Utilisé depuis 3 ans au sein du pôle registration de CYIM, avec l'utilisation de requêtes, de mutations et de diverses librairies.",
+      experience: t('skillsPage.vueExp'),
       startBlock: 'slide-right'
     },
     {
@@ -68,8 +73,17 @@ import { ref, onMounted, nextTick } from 'vue';
         '/assets/images/icon/js.png'
       ],
       percentage: 100,
-      experience: "Utilisé depuis 8 ans sur différents projets personnels et professionnels, au cours de mes périodes de formation, de stages et d'emplois.",
+      experience: t('skillsPage.baseExp'),
       startBlock: 'slide-left'
+    },
+    {
+      name: "symfony",
+      logo: [
+        '/assets/images/icon/symfony.png',
+      ],
+      percentage: 60,
+      experience: t('skillsPage.symfonyExp'),
+      startBlock:"slide-right"
     },
     {
       name: 'Flutter',
@@ -77,8 +91,8 @@ import { ref, onMounted, nextTick } from 'vue';
         '/assets/images/icon/flutter.png'
       ],
       percentage: 50,
-      experience: "Utilisé depuis 1 an dans un projet de registration pour développer une application Flutter Android sur tablette, gérée via un Raspberry Pi, permettant l'impression de badges après le scan d'un QR code sur la machine.",
-      startBlock: 'slide-right'
+      experience: t('skillsPage.flutterExp'),
+      startBlock: 'slide-left'
     },
     {
       name: 'Git & Jira',
@@ -87,7 +101,7 @@ import { ref, onMounted, nextTick } from 'vue';
         '/assets/images/icon/jira.png'
       ],
       percentage: 100,
-      experience: "Utilisés depuis 8 ans sur divers projets personnels et professionnels, au cours de mes périodes de formation, de stages et d'emplois.",
+      experience: t('skillsPage.otherExp'),
       startBlock: 'slide-right'
     }
   ])
@@ -110,8 +124,9 @@ onMounted(() => {
       // Séquence des étapes
       setTimeout(async() => {
         animatedSkills.value[index].showLogo = true
-        await nextTick()
-        scrollToSkill(index)
+        nextTick(() => {
+          scrollToBottom()
+        })
 
         setTimeout(() => {
           animatedSkills.value[index].showBar = true
@@ -138,169 +153,13 @@ function animatePercentage(index, target) {
   }, 15) // vitesse d'animation
 }
 
-function scrollToSkill(index) {
-  console.log();
-  
-  const el = skillRefs.value[index]
-  if (el) {
-    el.scrollIntoView({ behavior: 'smooth', block: 'center' })
+function scrollToBottom() {
+  if (scrollAnchor.value) {
+    scrollAnchor.value.scrollIntoView({ behavior: 'smooth', block: 'end' });
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  @use '../scss/index' as *;
-  .skills-container {
-    display: flex;
-    flex-direction: column;
-    gap: 2.5rem;
-    min-height: 90vh;
-  }
-
-  .skill-card {
-    padding: 1.5rem;
-    text-align: center;
-    overflow: hidden;
-  }
-
-  .list-icon {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    align-items: center;
-    gap: 2rem;
-  }
-
-  .logo-container {
-    display: flex;
-    justify-content: center;
-    margin: 1rem 0;
-    background-color: $white;
-    width: 100px;
-    height: 100px;
-    padding: 1.2rem;
-    border-radius: 50%;
-  }
-
-  /* Bounce In Animation */
-  .bounce-in {
-    animation: bounceIn 0.6s both;
-  }
-
-  @keyframes bounceIn {
-    0% {
-      opacity: 0;
-      transform: scale(0.3);
-    }
-    50% {
-      opacity: 1;
-      transform: scale(1.05);
-    }
-    70% {
-      transform: scale(0.9);
-    }
-    100% {
-      transform: scale(1);
-    }
-  }
-
-  /* Progress Bar */
-  .progress-bar-container {
-    margin: 1rem 0;
-  }
-
-  .progress-bar-bg {
-    width: 60%;
-    margin: auto;
-    height: 12px;
-    background: $white;
-    border-radius: 6px;
-    overflow: hidden;
-  }
-
-  .progress-bar-fill {
-    height: 100%;
-    background: $primary;
-    transition: width 0.3s ease;
-  }
-
-  /* Fade In Text */
-  // .fade-in {
-  //   animation: fadeIn 1s forwards;
-  // }
-
-  .experience-text {
-    background-color: $white;
-    width: 70%;
-    margin: 1rem auto;
-    padding: 1rem;
-    border-radius: 4px;
-    box-shadow: 0 1px 3px $gray--2;
-
-    &.slide-right {
-    -webkit-animation: slide-in-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-    animation: slide-in-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both; 
-    }
-
-    &.slide-left {
-      -webkit-animation: slide-in-left 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-      animation: slide-in-left 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-    }
-  }
-
-  @keyframes fadeIn {
-    from { opacity: 0; }
-    to { opacity: 1; }
-  }
-
-  @-webkit-keyframes slide-in-left {
-    0% {
-      -webkit-transform: translateX(-1000px);
-              transform: translateX(-1000px);
-      opacity: 0;
-    }
-    100% {
-      -webkit-transform: translateX(0);
-              transform: translateX(0);
-      opacity: 1;
-    }
-  }
-  @keyframes slide-in-left {
-    0% {
-      -webkit-transform: translateX(-1000px);
-              transform: translateX(-1000px);
-      opacity: 0;
-    }
-    100% {
-      -webkit-transform: translateX(0);
-              transform: translateX(0);
-      opacity: 1;
-    }
-  }
-
-  @-webkit-keyframes slide-in-right {
-    0% {
-      -webkit-transform: translateX(1000px);
-              transform: translateX(1000px);
-      opacity: 0;
-    }
-    100% {
-      -webkit-transform: translateX(0);
-              transform: translateX(0);
-      opacity: 1;
-    }
-  }
-  @keyframes slide-in-right {
-    0% {
-      -webkit-transform: translateX(1000px);
-              transform: translateX(1000px);
-      opacity: 0;
-    }
-    100% {
-      -webkit-transform: translateX(0);
-              transform: translateX(0);
-      opacity: 1;
-    }
-  }
-
+    @forward "../scss/components/skillsPage.scss"
 </style>
